a36c0a85db6a79a269dcbcbec160bd6fb2fb0d93
leonardoFerreira21 <leoboss9992@gmail.com>
2023-05-16 14:09:39 -0300

    "comments and some changes in code"

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+/* 
+Basicão JavaScript
+*/
+
+
+//Escreve no console
 console.log('Hello World!');
 
+//Exemplo de declaração de função
 function teste(i,j){
+   
+   //Variáveis no js: let, var e const
+   const constante = 1;
+   var str = 'Leonardo';
+   let result = i+j;
 
-   var result = i+j;
-
-   document.write(result);
+   //Typeof: printa o tipo de variável
+   console.log(typeof(result));
+   console.log(result);
 
 }
 
 
-
+//chamando função
 teste(4,3);
+
