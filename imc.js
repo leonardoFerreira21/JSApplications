0ed5fcfc8eaac8f2eb54512a9ed09bd52b69192d
leonardoFerreira21 <leoboss9992@gmail.com>
2023-07-03 12:15:10 -0300

    added imc

---
+++ imc.js
@@ -0,0 +1,11 @@
+const nome = 'Leonardo';
+const sobrenome = 'Ferreira';
+const idade = 24;
+const altura = 1.80;
+const peso = 60;
+
+
+let imc = peso / (altura * altura);
+let anoNasc = 2023 - idade;
+console.log(imc);
+console.log(anoNasc);
